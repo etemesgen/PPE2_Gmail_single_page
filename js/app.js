ac185fae72780f7234fdd7fea0367acc0bcfd121
Edomiyas Temesgen <temesgen.edomiyas@yahoo.com>
2021-03-04 17:46:25 +0100

    HTML CSS done but responsive nav bar error and JS to do

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/* Element pour message d'invalidation 
+
+let nom = document.getElementsByTagName("input")[4]
+let expr = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/;
+//créer un élément virtuel dans le DOM 
+ let parent = document.querySelector("fieldset")
+ let el = document.createElement("p")
+ console.log(el)
+ 
+ parent.prepend(el)
+ el.innerText = "hello"
+ submit.addEventeListener("click",(e)=>{
+ 	e.preventDefault()
+ 
+ 		//ERREUR 405;
+ 		//appel de la fonction
+ })
+*/
+ 
